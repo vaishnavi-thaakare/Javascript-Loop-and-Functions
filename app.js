4eee0e40fc51c73cbe991064899e41d7814e2b3a
vaishnavithakare2807 <vaishnavithaakare@gmail.com>
2022-02-28 09:52:29 +0530

    add repeat function

---
+++ app.js
@@ -0,0 +1,12 @@
+// function repeat(personName, number) {
+//     let result = "  ";
+//     for (let i = 0; i < number; i = i + 1) {
+//         result += personName;
+//     }
+//     console.log(result);
+// }
+
+// repeat("Vaishnavi", 5)
+
+
+
